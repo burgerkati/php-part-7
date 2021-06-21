9c6309ab7c2807ef8d450e47b4686ea2ad39363f
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-06-21 11:54:11 +0200

    add finished exercise w/o style

---
+++ exercice_2/index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP pt. 7 exercice_2</title>
+  </head>
+  <body>
+    <?php
+    $_SESSION['island'] = 'Île d\'Oléron';
+    $_SESSION['seashell'] = 'coquille';
+    echo 'J\'aimerais trouver une ' . $_SESSION['seashell'] . ' de palourde sur l\'' . $_SESSION['island'] . '.';
+    ?>
+  </body>
+</html>
