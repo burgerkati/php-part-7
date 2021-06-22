c10b3c74987c54015d8d8d621bd4d848f1e5b65c
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-06-22 14:10:03 +0200

    add finished exercise w/o style

---
+++ exercice_3/index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP pt. 7 exercice_3</title>
+  </head>
+  <!-- Créez trois variables de session (nom, prénom et âge). Affichez l'âge sur une autre page en cliquant sur un lien. -->
+  <body>
+    <?php
+    $_SESSION['surname'] = 'Schmidt';
+    $_SESSION['name'] = 'Julia';
+    $_SESSION['age'] = '45';
+    $newPage = fopen('user.php', 'a+');
+    fclose($newPage);
+    ?>
+    <a href="user.php">Link to user page</a>
+  </body>
+</html>
---
+++ exercice_3/user.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+?>
+<?php
+echo 'The session variable age is: ' . $_SESSION['age'] = 45;
+?>
